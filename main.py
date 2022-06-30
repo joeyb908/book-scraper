2fed16a1619c93e09698651a451daf77abaaf584
joeyb908@gmail.com <AckeSIMEYEFrIciO>
2022-06-30 08:44:21 -0400

    initial creation of API data. The following are completed:
    title,
    author,
    chapter,
    tags

--- main.py
+++ main.py
@@ -4,25 +4,53 @@ import requests
 import pprint
 from flask import Flask
 
-# pp = pprint.PrettyPrinter(indent=4, compact=True)
-# BASE_URL = 'https://www.royalroad.com/'
-# URL = 'https://www.royalroad.com/fiction/21220/mother-of-learning'
-# response = requests.get(url=URL)
-# webpage = response.text
-# soup = BeautifulSoup(webpage, 'lxml')
-# # pp.pprint(soup)
-#
-# chapter_list = []
-# chapters = soup.findAll(class_='chapter-row')
-# for chapter in chapters:
-#     head, sep, tail = chapter.get_text().strip().partition('\n')
-#     link = chapter.find(href=True)
-#     appended_link = f'{BASE_URL}{link["href"]}'
-#     chapter_list.append([head, appended_link])
-#     # pp.pprint(link['href'])
-#     # pp.pprint(head)
-#
-# pp.pprint(chapter_list)
+pp = pprint.PrettyPrinter(indent=4, compact=True)
+BASE_URL = 'https://www.royalroad.com/'
+URL = 'https://www.royalroad.com/fiction/21220/mother-of-learning'
+response = requests.get(url=URL)
+webpage = response.text
+soup = BeautifulSoup(webpage, 'lxml')
+# pp.pprint(soup)
+
+# finds book title
+title = soup.find(property='name', ).get_text()
+
+# find author using selectors
+# a list gets returned even though it's only a single index
+author = soup.select_one('.col > h4 > :last-child > a').get_text()
+
+# finds all the tags
+found_tags = soup.findAll(class_='tags')
+# creates list of tags
+tags = [(tag.get_text().strip('\n').splitlines()) for tag in found_tags]
+# 'unpacks list of tags' into a single dimension list as returned
+# value from the comprehension was a nested empty list
+[tags] = tags
+
+# finds all chapters and accompanying links
+chapter_list = []
+chapters = soup.findAll(class_='chapter-row')
+for chapter in chapters:
+    # pulls only the chapter name into head value
+    head, sep, tail = chapter.get_text().strip().partition('\n')
+
+    # pulls the chapter link
+    link = chapter.find(href=True)
+    appended_link = f'{BASE_URL}{link["href"]}'
+    chapter_list.append([head, appended_link])
+
+    published_date = chapter.find('time').get_text()
+    # pp.pprint(published_date)
+
+book_info = {
+    'title': title,
+    'author': author,
+    'chapter': chapter_list,
+    'tags': tags,
+
+}
+
+pp.pprint(book_info)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -32,5 +60,6 @@ app.config['DEBUG'] = True
 def home():
     return "<p>Hello, World!</p>"
 
-
-
+#
+# if __name__ == '__main__':
+#     app.run()
