c917bd5c3bfd2bcfc302bb53ba0d4ae3fc59eaae
joeyb908@gmail.com <AckeSIMEYEFrIciO>
2022-07-01 10:17:24 -0400

    Specify specific version of packages needed

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
 
 setup(name='book-scraper',
-      packages=['beautifulsoup4', 'Flask', 'lxml', 'requests'])
+      version='.1',
+      description='A prototype book-scraper',
+      url='https://github.com/joeyb908/book-scraper',
+      author='joeyb908',
+      author_email='joey@ohannesian.io',
+      license='MIT',
+      install_requires=['beautifulsoup4==4.11.1',
+                        'flask==2.1.2',
+                        'lxml==4.9.0',
+                        'requests==2.28.1'],
+      )
