dca1e39e660e7e25a8dd13d33f6b4f57220e19a7
joeyb908@gmail.com <AckeSIMEYEFrIciO>
2022-07-01 10:40:39 -0400

    Move setup.py to its own folder

--- setup/setup.py
+++ setup/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='book-scraper',
-      version='.1',
+      version='0.1',
       description='A prototype book-scraper',
       url='https://github.com/joeyb908/book-scraper',
       author='joeyb908',
