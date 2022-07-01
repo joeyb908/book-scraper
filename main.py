94bc3a834170a74dcc9bab6b3327aac9621d5f40
joeyb908@gmail.com <AckeSIMEYEFrIciO>
2022-07-01 14:17:12 -0400

    - Removal of some comments and refactored returns for functions for easier readability
    - Removal of pprint package as it's no longer needed

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from bs4 import BeautifulSoup
 import lxml
 import requests
-import pprint
 from flask import Flask, jsonify, request
 
-# pp = pprint.PrettyPrinter(indent=4, compact=True)
 BASE_URL = 'https://www.royalroad.com/'
 book_info = []
 
@@ -30,14 +28,8 @@ def find_author(soup):
 def find_tags(soup):
     """Finds all tags and returns them as a list"""
 
-    # Initial search for raw tag html
     found_tags = soup.findAll(class_='tags')
-
-    # Formats tags into a list
     tags = [(tag.get_text().strip('\n').splitlines()) for tag in found_tags]
-
-    # Unpacks list of tags into a single dimension list because the returned value from the list comprehension
-    # was a nested empty list; example: [['Adventure', 'Thriller']]
     [tags] = tags
     return tags
 
@@ -46,18 +38,12 @@ def find_chapters(soup):
     """Finds all chapters and accompanying links"""
 
     chapter_list = []
-
-    # Locate the location of the webpage where chapters are housed
     chapters = soup.findAll(class_='chapter-row')
     for chapter in chapters:
         # Separates the chapter name from the 'junk' that is returned
         head, sep, tail = chapter.get_text().strip().partition('\n')
-
-        # Pulls the chapter link
         link = chapter.find(href=True)
         appended_link = f'{BASE_URL}{link["href"]}'
-
-        # Appends each chapter and accompanying link to chapter list.
         chapter_list.append([head, appended_link])
     return chapter_list
 
@@ -69,32 +55,25 @@ def find_stats(soup):
     rating = soup.find(property='ratingValue').attrs['content']
     rate_count = soup.find(property='ratingCount').attrs['content']
 
-    # Finds total views, followers, favorites, and pages
     stats = soup.select('.stats-content > :last-child > ul > :nth-child(even)')
     views = stats[0].get_text()
     followers = stats[2].get_text()
     favorites = stats[3].get_text()
     pages = stats[5].get_text()
 
-    # Return the whole list of stats to be processed inside book_info
-    list_of_stats = [rating, rate_count, views, followers, favorites, pages]
-    return list_of_stats
+    return [rating, rate_count, views, followers, favorites, pages]
 
 
 def create_book_info(book_link):
     """Creates dictionary with all relevant book information"""
 
-    # Creates Soup with provided book link
     soup = create_soup(book_link)
-
-    # Finds the title, author, tags, chapter list, views, followers, favorites, and total pages
     title = find_title(soup)
     author = find_author(soup)
     tags = find_tags(soup)
     chapter_list = find_chapters(soup)
     list_of_stats = find_stats(soup)
 
-    # Appends stats to the book info list
     book_info.append({
         'title': title,
         'author': author,
@@ -111,7 +90,8 @@ def create_book_info(book_link):
 
 
 def return_single_book():
-    """Returns a single book from book_info if book_info has more than one book within it"""
+    """Returns a single book from book_info if book_info has more than one book within it. Doesn't need to return a
+        value because it appends to a list that's global"""
 
     # Create an empty results tag that will be unmodified if a book is not found
     results = []
@@ -134,19 +114,16 @@ def return_single_book():
 def search_for_book():
     """Searches https://www.royalroad.com/ for entered book"""
 
-    # Searches royalroad for the book
     URL = f'https://www.royalroad.com/fictions/search?title={request.args["title"]}'
 
-    # Create the Soup and pulls the link for the book's page
+    # Pulls the link for the book's page
     soup = create_soup(URL)
     book_link = soup.find(class_='fiction-title')
     book_link = book_link.find(href=True)
     book_link = f'https://www.royalroad.com{book_link["href"]}'
 
-    # Pulls the book's information from its webpage and returns the results
     create_book_info(book_link)
-    results = return_single_book()
-    return results
+    return return_single_book()
 
 
 # Flask app creation in debug mode
@@ -163,13 +140,15 @@ def api_base():
 
 @app.route('/api/v1/books/all')
 def api_all():
-    # Provide JSON for all of the scraped books
+    # Provide JSON for all the scraped books
     return jsonify(book_info)
 
 
 # set routing for title of book
 @app.route("/api/v1/books", methods=['GET'])
 def api_book():
+    """Returns JSON of the searched book"""
+
     # Check if a title was provided as part of the URL.
     # If title is a parameter, see if it's located within the saved book info list
     # If not, search for the book online and add it to the book info
@@ -184,8 +163,6 @@ def api_book():
     else:
         return "Error: No title field provided. Please specify a title."
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
     return jsonify(results)
 
 
