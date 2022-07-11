5884b0ec4eb3d8e3b1d687af14ed96368d77151c
joeyb908@gmail.com <AckeSIMEYEFrIciO>
2022-07-11 14:28:53 -0400

    add SQLAlchemy to setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -10,5 +10,6 @@ setup(name='book-scraper',
       install_requires=['beautifulsoup4==4.11.1',
                         'flask==2.1.2',
                         'lxml==4.9.0',
-                        'requests==2.28.1'],
+                        'requests==2.28.1',
+                        'Flask-SQLAlchemy==2.5.1'],
       )
