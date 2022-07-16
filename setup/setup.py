fa43a24c15fc4a1124ed56526bd18d4e0b290eab
joeyb908@gmail.com <AckeSIMEYEFrIciO>
2022-07-16 13:17:18 -0400

    Updated settings to include SQL alchemy

--- setup/setup.py
+++ setup/setup.py
@@ -10,6 +10,6 @@ setup(name='book-scraper',
       install_requires=['beautifulsoup4==4.11.1',
                         'flask==2.1.2',
                         'lxml==4.9.0',
-                        'requests==2.28.1',
-                        'Flask-SQLAlchemy==2.5.1'],
+                        'requests==2.28.1'
+                        'sqlalchemy==1.4.39'],
       )
